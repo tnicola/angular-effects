5d15858e0fe2351e55f16ceca617b8056c4a582e
Nicola Tommasi <nicola.tommasi@fleetmatics.com>
2020-01-29 12:47:50 +0100

    first-commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import './polyfills';
+import 'reflect-metadata';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+if (process.env.NODE_ENV === 'production') {
+  enableProdMode();
+}
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
